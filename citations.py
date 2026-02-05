31dc3530050b5a1c3c9cf5214ffe774933b246a6
Binh <105949566+chickenflavoured@users.noreply.github.com>
2026-02-05 14:20:57 -0500

    Add files via upload

--- citations.py
+++ citations.py
@@ -3,6 +3,20 @@ from tkinter import ttk
 import pyperclip
 
 
+def get_entry():
+    author = f"{ln.get()}, {fn.get()}"
+    f.config(state="normal")
+    f.delete(1.0, tk.END)
+    f.insert(1.0, author)
+    f.config(state="disabled")
+
+
+def select_type(event):
+
+    if sourcetype.get(1) == "Book":
+        print(sourcetype.get(0))
+        tk.Label(root, text="Word").grid(row=5, column=5)
+
 
 root = tk.Tk()
 root.geometry(f"960x720")
@@ -34,7 +48,8 @@ sourcetypes = (
     "Other"
 )
 
-sourcetype = tk.Listbox(root, listvariable=tk.Variable(value=sourcetypes), height=len(sourcetypes))
+sourcetype = tk.Listbox(root, listvariable=tk.Variable(value=sourcetypes), selectmode="browse", height=len(sourcetypes))
+sourcetype.bind("<<ListboxSelect>>", select_type)
 sourcetype.grid(row=2, column=0, padx=(20, 20))
 
 
@@ -42,9 +57,24 @@ tk.Label(root, text="Author's first name").grid(row=3, column=2)
 tk.Label(root, text="Author's middle name").grid(row=4, column=2)
 tk.Label(root, text="Author's last name").grid(row=5, column=2)
 
-fn = tk.Entry(root).grid(row=3, column=3)
+fn = tk.Entry(root)
+fn.grid(row=3, column=3)
+
 mn = tk.Entry(root).grid(row=4, column=3)
-ln = tk.Entry(root).grid(row=5, column=3)
+
+ln = tk.Entry(root)
+ln.grid(row=5, column=3)
+
+textframe = tk.Frame(root)
+textframe.grid(row=6, column=0)
+
+
+f = tk.Text(textframe, wrap="word", width=20, height=10, state="disabled")
+f.insert(1.0, "hello")
+f.grid(row=1, column=1)
+
+button = tk.Button(root, text="Get Entry Value", command=get_entry)
+button.grid(row=6, column=2)
 
 
 root.mainloop()
