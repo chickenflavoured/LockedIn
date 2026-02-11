6ca6b68a9af42e6e8097050b266c9b988da043e1
Carovolyn <czhan1@ocdsb.ca>
2026-02-11 14:18:56 -0500

    Add files via upload

--- studyspace.py
+++ studyspace.py
@@ -10,37 +10,41 @@ WIDTH = 600
 # Window display size
 window.geometry(f"{WIDTH}x{HEIGHT}")
 
-def start_timer(countdown_time, running):
-
-
-    print(countdown_time, running)
-    timer = Timer(countdown_time, running)
-    timer.countdown(window)
-
-
-
-# Thread that executes start_timer separately so that main can also continue to execute
-def start_timer_thread(time_arg, run_arg, pause):
-
-    # Somethign with this pause thread stuff doesn't work
-    pause_thread = threading.Event
-    pause_thread.set
-
-    separate = threading.Thread(target = start_timer, args = (time_arg, run_arg))
+def start_timer_thread(timer, time, run):
+    
+    timer.countdown_time = time
+    timer.running = run
+    separate = threading.Thread(target = timer.countdown, args = (window,))
     separate.start()
+    
 
-    while not pause:
-        pause_thread.wait()
+def pause_timer_thread(timer):
+    
+    print("paused")
+    timer.pause_event.clear()
+
+def resume_timer_thread(timer):
+
+    timer.pause_event.set()
+
+time = 0
+run = False
+
+timer = Timer(time, run)
 
 time_var = tk.IntVar()
 
 time_label = tk.Label(window, text = "Time")
 time_entry = tk.Entry(window, textvariable = time_var)
 
-start_btn = tk.Button(window, text = "Begin Timer", command = lambda: start_timer_thread(time_var.get(), True)) # Lambda allows command to pass arguments without immediately executing function
-pause_btn = tk.Button(window, text = "Pause", command = lambda: start_timer_thread(time_var.get(), True, False))
+crt_time_lst = [time_var.get()]
+
+start_btn = tk.Button(window, text = "Begin Timer", command = lambda: start_timer_thread(timer, time_var.get(), True)) # Lambda allows command to pass arguments without immediately executing function
+pause_btn = tk.Button(window, text = "Pause", command = lambda: pause_timer_thread(timer))
+resume_btn = tk.Button(window, text = "Resume", command = lambda: resume_timer_thread(timer))
 
 pause_btn.grid(row = 2, column = 0)
+resume_btn.grid(row = 3, column = 0)
 time_label.grid(row = 0, column = 0)
 time_entry.grid(row = 0, column = 1)
 start_btn.grid(row = 1, column = 0)
--- studyspace_timers.py
+++ studyspace_timers.py
@@ -1,37 +1,43 @@
+import tkinter as tk
+from tkinter import ttk
 import time
 import sys
+import threading
 
 class Timer:
     def __init__(self, countdown_time:int, timer_running:bool):
         self.countdown_time = countdown_time * 60
         self.timer_running = timer_running
+        self.pause_event = threading.Event
+        self.pause_event.set()
 
-    def countdown(self):
-    
+    def countdown(self, window):
+        
+        timer_display = tk.Label(window)
+        timer_display.grid(row = 4, column = 4)
         # Main timer loop
-        while self.countdown_time >= 0 and self.timer_running == True:
+        while self.countdown_time >= 0:
+
+            self.pause_event.wait()
             
             hour = int(self.countdown_time // 3600)
             min = int(self.countdown_time // 60)
             sec = int(self.countdown_time % 60)
             
             # Print amount of time left
-            print(f"\rThere's {hour}:{min}:{sec} left", end ="", flush = True )
+            timer_display.config(text = f"There's {hour}:{min}:{sec} left")
 
             # Let the second pass
             time.sleep(1)
 
             # Reduce timer
-            self.countdown_time -= 1    
-
-        print("Time's up")
-
+            self.countdown_time -= 1  
+ 
 
     def pomodoro(self):
         pass
 
     def stopwatch(self):
         pass
-
 
 
