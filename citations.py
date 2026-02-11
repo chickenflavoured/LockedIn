7195659a6845f271c231651874b459d6d40748a3
Binh <105949566+chickenflavoured@users.noreply.github.com>
2026-02-11 14:18:14 -0500

    Update citations.py

--- citations.py
+++ citations.py
@@ -17,10 +17,9 @@ def select_type(event):
     for widget in fields.winfo_children():
         widget.grid_remove()
 
-    for i, widget in enumerate(sourcetypes[option]):
-        widget.grid(row=i, column=0)
-
-        
+    for i, field in enumerate(sourcetypes[option]):
+        for j, widget in (enumerate(field)):
+            widget.grid(row=i, column=j)
 
 
 root = tk.Tk()
@@ -31,50 +30,129 @@ frm.grid()
 
 test = tk.Label(frm, text="citation generator")
 test.grid(row=0, column=0)
-print(test)
 
 fields = tk.Frame(root)
-fields.grid(row=7, column=0)
-
+fields.grid(row=8, column=0)
 
 
 sourcetypes = {
     "Book": [
-        tk.Label(fields, text="Word")
+
+        [tk.Label(fields, text="Title"),
+        tk.Entry(fields)],
+
+        [tk.Label(fields, text="Subtitle"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Publisher"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="DOI"),
+         tk.Entry(fields)]
 
     ],
 
     "Article": [
-    ],
+        [tk.Label(fields, text="Article title"),
+         tk.Entry(fields)],
 
-    "Periodical": [
-    ],
+        [tk.Label(fields, text="Journal name"),
+         tk.Entry(fields)],
 
-    "Scholarly journal": [
-    ],
+        [tk.Label(fields, text="Volume No."),
+         tk.Entry(fields)],
 
-    "Dissertation or thesis": [
+        [tk.Label(fields, text="Issue No."),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Start page"),
+         tk.Entry(fields),
+         tk.Label(fields, text="End page"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="DOI"),
+         tk.Entry(fields)]
     ],
 
     "Website": [
+        [tk.Label(fields, text="Page title"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Website name"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Date accessed"),
+         tk.Entry(fields)]
+
     ],
 
     "Blog post": [
+        [tk.Label(fields, text="Post title"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Blog name"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Version number"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Organization name"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Date accessed"),
+         tk.Entry(fields)]
     ],
 
     "Social media": [
+        [tk.Radiobutton(fields, text="Post", value="post"),
+         tk.Radiobutton(fields, text="Comment", value="comment")],
+
+        [tk.Label(fields, text="Author handle"),
+         tk.Entry(fields)],
+
+         [tk.Label(fields, text="Content"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Time"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Website name"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Date accessed"),
+         tk.Entry(fields)],
     ],
 
     "Video": [
-    ],
+        [tk.Label(fields, text="Video title"),
+         tk.Entry(fields)],
 
-    "Image": [
+        [tk.Label(fields, text="Website name"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Uploader"),
+        tk.Entry(fields)],
     ],
 
     "Artwork": [
+        [tk.Label(fields, text="Title"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Medium"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="Location/source"),
+         tk.Entry(fields)],
+
+        [tk.Label(fields, text="City"),
+         tk.Entry(fields),
+         tk.Label(fields, text="Country"),
+         tk.Entry(fields)]
     ],
 
     "Music": [
+        [tk.Label(fields, text="Location/source"),
+         tk.Entry(fields)],
     ],
 
     "Podcast": [
@@ -83,13 +161,13 @@ sourcetypes = {
     "Speech or interview": [
     ],
 
+    "Thesis or dissertation": [
+    ],
+
     "Court case": [
     ],
 
     "Religious text": [
-    ],
-
-    "Map": [
     ],
 
     "Other": [
@@ -97,32 +175,42 @@ sourcetypes = {
 
 }
 
-sourcetype = tk.Listbox(root, listvariable=tk.Variable(value=list(sourcetypes.keys())), selectmode="browse", height=len(sourcetypes))
+sourcetype = tk.Listbox(root, listvariable=tk.Variable(value=list(sourcetypes.keys())), selectmode="browse", height=len(sourcetypes), exportselection=False)
 sourcetype.bind("<<ListboxSelect>>", select_type)
-sourcetype.grid(row=1, column=0, padx=(20, 20))
+sourcetype.grid(row=1, column=0, padx=(50, 20))
 
 
 tk.Label(root, text="Author's first name").grid(row=3, column=2)
 tk.Label(root, text="Author's middle name").grid(row=4, column=2)
 tk.Label(root, text="Author's last name").grid(row=5, column=2)
+tk.Label(root, text="Publication date").grid(row=4, column=4)
+tk.Label(root, text="URL").grid(row=5, column=4)
+
 
 fn = tk.Entry(root)
 fn.grid(row=3, column=3)
 
-mn = tk.Entry(root).grid(row=4, column=3)
+mn = tk.Entry(root)
+mn.grid(row=4, column=3)
 
 ln = tk.Entry(root)
 ln.grid(row=5, column=3)
 
+date = tk.Entry(root)
+date.grid(row=4, column=5)
+
+url = tk.Entry(root)
+url.grid(row=5, column=5)
+
 textframe = tk.Frame(root)
-textframe.grid(row=1, column=4)
+textframe.grid(row=1, column=9)
 
 
 f = tk.Text(textframe, wrap="word", width=20, height=10, state="disabled")
 f.grid(row=0, column=0)
 
 button = tk.Button(root, text="Get Entry Value", command=get_entry)
-button.grid(row=6, column=2)
+button.grid(row=7, column=2)
 
 
 root.mainloop()
