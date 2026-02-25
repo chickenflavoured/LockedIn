788d83e6d43bd56de61c73eb3e3ac204e546e278
Carovolyn <czhan1@ocdsb.ca>
2026-02-25 14:19:27 -0500

    Add files via upload

--- studyspace.py
+++ studyspace.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from studyspace_timers import Timer
+from timers import Timer
 import threading
 
 window = tk.Tk()
@@ -10,7 +10,7 @@ WIDTH = 600
 # Window display size
 window.geometry(f"{WIDTH}x{HEIGHT}")
 
-def start_timer_thread(timer, time1, time2, time3):
+def start_timer_thread(timer, time1, time2, time3, time_begun, btn):
     """
     Starts running the countdown timer with a thread.
 
@@ -21,30 +21,46 @@ def start_timer_thread(timer, time1, time2, time3):
         time3(int)
 
     """
-    timer.countdown_time = time1 + (time2*60) + (time3*3600)
+
+    timer.total_time = time1 + (time2*60) + (time3*3600)
+    timer.running = time_begun[2]
     # Start running the thread with instance method
-    separate = threading.Thread(target = timer.countdown, args = (window,))
+    separate = threading.Thread(target = timer.countdown, args = (window,), daemon = True) # Needs to be a comma at args = (window,)
     separate.start()
+
+    if time_begun[1]:
+        time_begun[1] = False
+        btn.config(text = "Begin Timer")
+
+    else:
+        time_begun[1] = True
+        time_begun[2] = False
+        btn.config(text = "End Timer") 
     
-
-def pause_timer_thread(timer):
+def pause_timer_thread(timer, time_paused, btn):
     
-    print("paused")
-    timer.pause_event.clear() #.clear() pauses the thread attribute in timer
-
-def resume_timer_thread(timer):
-
-    print("resume")
-    timer.pause_event.set() #.set() resumes the thread attribute in timer
+    if not time_paused[0]:
+        time_paused[0] = True
+        btn.config(text = "Resume")
+        timer.pause_event.clear() #.clear() pauses the thread attribute in timer
+    else:
+        time_paused[0] = False
+        btn.config(text = "Pause")
+        timer.pause_event.set() #.set() resumes the thread attribute in timer
 
 # Create instance of Timer Class
 time = 0
 timer = Timer(time)
 
+# Variable keeping track of the timer.
+# [0] -> Whether the user is pausing or resuming the timer
+# [1] -> Whether the user is beginning or ending the timer.
+time_paused_and_started = [False, True, True]
+
 # Variables
-time_var1 = tk.IntVar()
-time_var2 = tk.IntVar()
-time_var3 = tk.IntVar()
+time_var1 = tk.IntVar(value = 0) 
+time_var2 = tk.IntVar(value = 0) 
+time_var3 = tk.IntVar(value = 0) 
 
 # Time entries and labels initialization
 time_label1 = tk.Label(window, text = "Seconds")
@@ -55,15 +71,13 @@ time_label3 = tk.Label(window, text = "Hours")
 time_entry3 = tk.Entry(window, textvariable = time_var3)
 
 # Buttons initialization
-start_btn = tk.Button(window, text = "Begin Timer", command = lambda: start_timer_thread(timer, time_var1.get(), time_var2.get(), time_var3.get())) # Lambda allows command to pass arguments without immediately executing function
-pause_btn = tk.Button(window, text = "Pause", command = lambda: pause_timer_thread(timer))
-resume_btn = tk.Button(window, text = "Resume", command = lambda: resume_timer_thread(timer))
+start_btn = tk.Button(window, text = "Begin Timer", command = lambda: start_timer_thread(timer, time_var1.get(), time_var2.get(), time_var3.get(), time_paused_and_started, start_btn)) # Lambda allows command to pass arguments without immediately executing function
+pause_btn = tk.Button(window, text = "Pause", command = lambda: pause_timer_thread(timer, time_paused_and_started, pause_btn))
 
 # -- Grid Placements --
 
 # Buttons
 pause_btn.grid(row = 4, column = 0)
-resume_btn.grid(row = 5, column = 0)
 start_btn.grid(row = 6, column = 0)
 
 # Labels
--- timers.py
+++ timers.py
@@ -8,32 +8,42 @@ class Timer:
     """
     Docstring for Timer
     """
-    def __init__(self, countdown_time:int):
-        self.countdown_time = countdown_time
+    def __init__(self, total_time:int):
+        self.time = total_time
         self.pause_event = threading.Event()
+        # total time become work time make new var total time + shor tbreak +long break
 
     def countdown(self, window):
+        """
+        Simple countdown total_timer based on the user's input
+
+        Args:
+            
+        """
         
         self.pause_event.set()
         timer_display = tk.Label(window)
         timer_display.grid(row = 4, column = 4)
-        # Main timer loop
-        while self.countdown_time >= 0:
 
-            self.pause_event.wait()
+        # Main timer loop
+        while self.total_time >= 0:
+
+            self.pause_event.wait() # this checks to see whether the event is set/cleared (running/paused)
             
-            hour = int(self.countdown_time // 3600)
-            min = int((self.countdown_time % 3600) // 60)
-            sec = int(self.countdown_time % 60)
+            hour = int(self.total_time // 3600)
+            min = int((self.total_time % 3600) // 60)
+            sec = int(self.total_time % 60)
             
-            # Print amount of time left
+            # Display amount of time left
             timer_display.config(text = f"There's {hour:02d}:{min:02d}:{sec:02d} left")
 
             # Let the second pass
             time.sleep(1)
 
             # Reduce timer
-            self.countdown_time -= 1  
+            self.total_time -= 1  
+        
+        print("timer over")
  
 
     def pomodoro(self):
