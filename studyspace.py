2fa2794b9699c36ff2c91180ef7f96c84f94bd05
Carovolyn <czhan1@ocdsb.ca>
2026-03-03 14:11:04 -0500

    Add files via upload

--- studyspace.py
+++ studyspace.py
@@ -10,9 +10,12 @@ WIDTH = 600
 # Window display size
 window.geometry(f"{WIDTH}x{HEIGHT}")
 
-def main(status, choice):
+def main(choice):
 
-    status[1] = choice
+    # Remove the buttons
+    pomodoro_btn.destroy()
+    countdown_btn.destroy()
+    stopwatch_btn.destroy()
 
     # Create instance of Timer Class
     time = 0
@@ -24,39 +27,60 @@ def main(status, choice):
     time_paused_and_started = [False, True, True]
 
     # Variables
-    time_var1 = tk.IntVar(value = 0) 
-    time_var2 = tk.IntVar(value = 0) 
-    time_var3 = tk.IntVar(value = 0) 
+    time_var = tk.IntVar(value = 0) 
+    rest_var = tk.IntVar(value = 0) 
+    longrest_var = tk.IntVar(value = 0)
+    session_var = tk.IntVar(value = 1)
 
-    # Time entries and labels initialization
-    time_label1 = tk.Label(window, text = "Seconds")
-    time_entry1 = tk.Entry(window, textvariable = time_var1)
-    time_label2 = tk.Label(window, text = "Minutes")
-    time_entry2 = tk.Entry(window, textvariable = time_var2)
-    time_label3 = tk.Label(window, text = "Hours")
-    time_entry3 = tk.Entry(window, textvariable = time_var3)
+    if choice == "pomodoro":
 
-    # Buttons initialization
-    start_btn = tk.Button(window, text = "Begin Timer", command = lambda: start_timer_thread(timer, time_var1.get(), time_var2.get(), time_var3.get(), time_paused_and_started, start_btn)) # Lambda allows command to pass arguments without immediately executing function
-    pause_btn = tk.Button(window, text = "Pause", command = lambda: pause_timer_thread(timer, time_paused_and_started, pause_btn))
+        # Time entries and labels initialization
+        rest_label = tk.Label(window, text = "Short rest (Minutes)")
+        longrest_label = tk.Label(window, text = "Long rest (Minutes)")
+        rest_entry = tk.Entry(window, textvariable = rest_var)
+        longrest_entry = tk.Entry(window, textvariable = longrest_var)
+        session_label = tk.Label(window, text = "Sessions")
+        session_entry = tk.Entry(window, textvariable = session_var)
 
-    # -- Grid Placements --
+        # Labels
+        rest_label.grid(row = 0, column = 2)
+        longrest_label.grid(row = 0, column = 4)
+        session_label.grid(row = 0, column = 6)
 
-    # Buttons
-    pause_btn.grid(row = 4, column = 0)
-    start_btn.grid(row = 6, column = 0)
+        # Entries
+        rest_entry.grid(row = 0, column = 3)
+        longrest_entry.grid(row = 0, column = 5)
+        session_entry.grid(row = 0, column = 7)
 
-    # Labels
-    time_label1.grid(row = 0, column = 0)
-    time_label2.grid(row = 2, column = 0)
-    time_label3.grid(row = 3, column = 0)
+    if choice == "pomodoro" or choice == "countdown":
+        # Time entries and labels initialization
+        time_label = tk.Label(window, text = "Minutes")
+        time_entry = tk.Entry(window, textvariable = time_var)
 
-    # Entries
-    time_entry1.grid(row = 0, column = 1)
-    time_entry2.grid(row = 2, column = 1)
-    time_entry3.grid(row = 3, column = 1)
+        # Buttons initialization
+        start_btn = tk.Button(window, text = "Begin Timer", command = lambda: start_timer_thread(timer, time_var.get(), rest_var.get(), longrest_var.get(), session_var.get(), time_paused_and_started, start_btn)) # Lambda allows command to pass arguments without immediately executing function
+        pause_btn = tk.Button(window, text = "Pause", command = lambda: pause_timer_thread(timer, time_paused_and_started, pause_btn))
 
-def start_timer_thread(timer, time1, time2, time3, time_begun, btn):
+        # -- Grid Placements --
+
+        # Buttons
+        pause_btn.grid(row = 4, column = 0)
+        start_btn.grid(row = 6, column = 0)
+
+        # Labels
+        time_label.grid(row = 0, column = 0)
+
+        # Entries
+        time_entry.grid(row = 0, column = 1)
+    
+    else:
+        new_btn = tk.Button(window, text = "Start", command = placeholder)
+        new_btn.grid(row = 0, column = 0)
+
+def placeholder():
+    print("This is a placeholder")
+
+def start_timer_thread(timer, work, rest, longrest, session, time_begun, btn):
     """
     Starts running the countdown timer with a thread.
 
@@ -68,7 +92,7 @@ def start_timer_thread(timer, time1, time2, time3, time_begun, btn):
 
     """
 
-    timer.total_time = time1 + (time2*60) + (time3*3600)
+    timer.total_time = 60*session*(work+rest) + longrest
     timer.running = time_begun[2]
     # Start running the thread with instance method
     separate = threading.Thread(target = timer.countdown, args = (window,), daemon = True) # Needs to be a comma at args = (window,)
@@ -94,11 +118,10 @@ def pause_timer_thread(timer, time_paused, btn):
         btn.config(text = "Pause")
         timer.pause_event.set() #.set() resumes the thread attribute in timer
 
-status = ["Inactive", "None"]
 
-pomodoro_btn = tk.Button(window, text = "Pomodoro", command = lambda: main(status, "pomodoro"))
-countdown_btn = tk.Button(window, text = "Countdown", command = lambda: main(status, "countdown"))
-stopwatch_btn = tk.Button(window, text = "Stopwatch", command = lambda: main(status, "stopwatch"))
+pomodoro_btn = tk.Button(window, text = "Pomodoro", command = lambda: main("pomodoro"))
+countdown_btn = tk.Button(window, text = "Countdown", command = lambda: main("countdown"))
+stopwatch_btn = tk.Button(window, text = "Stopwatch", command = lambda: main("stopwatch"))
 
 pomodoro_btn.grid(row = 2, column = 1)
 countdown_btn.grid(row = 2, column = 3)
