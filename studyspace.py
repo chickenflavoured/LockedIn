2c2604c839686f1b8d09f33687730fff6f3ff533
Carovolyn <czhan1@ocdsb.ca>
2026-03-02 22:35:32 -0500

    Update studyspace.py

--- studyspace.py
+++ studyspace.py
@@ -10,6 +10,52 @@ WIDTH = 600
 # Window display size
 window.geometry(f"{WIDTH}x{HEIGHT}")
 
+def main(status, choice):
+
+    status[1] = choice
+
+    # Create instance of Timer Class
+    time = 0
+    timer = Timer(time)
+
+    # Variable keeping track of the timer.
+    # [0] -> Whether the user is pausing or resuming the timer
+    # [1] -> Whether the user is beginning or ending the timer.
+    time_paused_and_started = [False, True, True]
+
+    # Variables
+    time_var1 = tk.IntVar(value = 0) 
+    time_var2 = tk.IntVar(value = 0) 
+    time_var3 = tk.IntVar(value = 0) 
+
+    # Time entries and labels initialization
+    time_label1 = tk.Label(window, text = "Seconds")
+    time_entry1 = tk.Entry(window, textvariable = time_var1)
+    time_label2 = tk.Label(window, text = "Minutes")
+    time_entry2 = tk.Entry(window, textvariable = time_var2)
+    time_label3 = tk.Label(window, text = "Hours")
+    time_entry3 = tk.Entry(window, textvariable = time_var3)
+
+    # Buttons initialization
+    start_btn = tk.Button(window, text = "Begin Timer", command = lambda: start_timer_thread(timer, time_var1.get(), time_var2.get(), time_var3.get(), time_paused_and_started, start_btn)) # Lambda allows command to pass arguments without immediately executing function
+    pause_btn = tk.Button(window, text = "Pause", command = lambda: pause_timer_thread(timer, time_paused_and_started, pause_btn))
+
+    # -- Grid Placements --
+
+    # Buttons
+    pause_btn.grid(row = 4, column = 0)
+    start_btn.grid(row = 6, column = 0)
+
+    # Labels
+    time_label1.grid(row = 0, column = 0)
+    time_label2.grid(row = 2, column = 0)
+    time_label3.grid(row = 3, column = 0)
+
+    # Entries
+    time_entry1.grid(row = 0, column = 1)
+    time_entry2.grid(row = 2, column = 1)
+    time_entry3.grid(row = 3, column = 1)
+
 def start_timer_thread(timer, time1, time2, time3, time_begun, btn):
     """
     Starts running the countdown timer with a thread.
@@ -48,46 +94,14 @@ def pause_timer_thread(timer, time_paused, btn):
         btn.config(text = "Pause")
         timer.pause_event.set() #.set() resumes the thread attribute in timer
 
-# Create instance of Timer Class
-time = 0
-timer = Timer(time)
+status = ["Inactive", "None"]
 
-# Variable keeping track of the timer.
-# [0] -> Whether the user is pausing or resuming the timer
-# [1] -> Whether the user is beginning or ending the timer.
-time_paused_and_started = [False, True, True]
+pomodoro_btn = tk.Button(window, text = "Pomodoro", command = lambda: main(status, "pomodoro"))
+countdown_btn = tk.Button(window, text = "Countdown", command = lambda: main(status, "countdown"))
+stopwatch_btn = tk.Button(window, text = "Stopwatch", command = lambda: main(status, "stopwatch"))
 
-# Variables
-time_var1 = tk.IntVar(value = 0) 
-time_var2 = tk.IntVar(value = 0) 
-time_var3 = tk.IntVar(value = 0) 
-
-# Time entries and labels initialization
-time_label1 = tk.Label(window, text = "Seconds")
-time_entry1 = tk.Entry(window, textvariable = time_var1)
-time_label2 = tk.Label(window, text = "Minutes")
-time_entry2 = tk.Entry(window, textvariable = time_var2)
-time_label3 = tk.Label(window, text = "Hours")
-time_entry3 = tk.Entry(window, textvariable = time_var3)
-
-# Buttons initialization
-start_btn = tk.Button(window, text = "Begin Timer", command = lambda: start_timer_thread(timer, time_var1.get(), time_var2.get(), time_var3.get(), time_paused_and_started, start_btn)) # Lambda allows command to pass arguments without immediately executing function
-pause_btn = tk.Button(window, text = "Pause", command = lambda: pause_timer_thread(timer, time_paused_and_started, pause_btn))
-
-# -- Grid Placements --
-
-# Buttons
-pause_btn.grid(row = 4, column = 0)
-start_btn.grid(row = 6, column = 0)
-
-# Labels
-time_label1.grid(row = 0, column = 0)
-time_label2.grid(row = 2, column = 0)
-time_label3.grid(row = 3, column = 0)
-
-# Entries
-time_entry1.grid(row = 0, column = 1)
-time_entry2.grid(row = 2, column = 1)
-time_entry3.grid(row = 3, column = 1)
+pomodoro_btn.grid(row = 2, column = 1)
+countdown_btn.grid(row = 2, column = 3)
+stopwatch_btn.grid(row = 2, column = 5)
 
 window.mainloop()
