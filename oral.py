c1aeb3f813c3836905c9179b0b3e6909a9ba7c8c
Binh <105949566+chickenflavoured@users.noreply.github.com>
2026-04-07 12:48:46 -0400

    Update oral.py

--- oral.py
+++ oral.py
@@ -1,11 +1,15 @@
 import tkinter as tk
-
+import sounddevice as sd
+import numpy as np
+from scipy.io import wavfile
 
 root = tk.Tk()
 root.title("Oral Presentation")
-root.geometry(f"600x720")
+root.geometry("600x720")
 
+samplerate = 44100
 
+recording = sd.rec(6 * samplerate, samplerate=samplerate, channels=2)
 
 
 root.mainloop()
