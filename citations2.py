30c74b27618c43add945201d473669d3c4c5742d
Carovolyn <czhan1@ocdsb.ca>
2026-04-09 14:10:15 -0400

    second version of citation

---
+++ citations2.py
@@ -0,0 +1,360 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+def get_entry():
+
+    # List where citation features are added
+    f.config(state="normal")
+    f.delete(1.0, tk.END)
+
+    # Temporary list that will hold all the citation features
+    temp_list = []
+
+    # Make another list - instead of f.insert - insert into another list. Then, use a for loop to run through that new list, and at the end insert the value into f list with a period or comma after 
+    # ^ this is how to get rid of the issue of the periods and commas because right now, if you leave an empty space, it will still input a comma/period
+
+    author = f"{ln.get()}, {fn.get()[0]}"
+    temp_list.insert(1, author)
+
+    time = f"{date.get()}"
+    temp_list.insert(2, time)
+
+    ititle = f"{variables["ititle"][0].get()}"
+    temp_list.insert(3, ititle)
+
+    title = f"{variables["title"][0].get()}"
+    temp_list.insert(3, title)
+
+    years = f"{variables["sy"][0].get()}-{variables["ey"][0].get()}"
+    temp_list.insert(4, years)
+
+    episode = f"{variables["episode"][0].get()}"
+    temp_list.insert(5, episode)
+
+    edition = f"{variables["edition"][0].get()}"
+    temp_list.insert(6, edition)
+
+    volume = f"{variables["volume"][0].get()}"
+    temp_list.insert(7, volume)
+
+    # Issue and volume are sometimes the same
+    issue = f"{variables["issue"][0].get()}"
+    temp_list.insert(7, issue)
+
+    type = f"{variables["type"][0].get()}"
+    temp_list.insert(8, type)
+
+    album = f"{variables["album"][0].get()}"
+    temp_list.insert(9, album)
+
+    source = f"{variables["source"][0].get()}"
+    temp_list.insert(10, source)
+
+    isource = f"{variables["isource"][0].get()}"
+    temp_list.insert(10, isource)
+
+    institution = f"{variables["institution"][0].get()}"
+    temp_list.insert(11, institution)
+
+    pub = f"{variables["pub"][0].get()}"
+    temp_list.insert(12, pub)
+
+    ipub = f"{variables["ipub"][0].get()}"
+    temp_list.insert(13, ipub)
+
+    pages = f"{variables["sp"][0].get()}-{variables["ep"][0].get()}"
+    temp_list.insert(14, pages)
+
+    location = f"{variables["city"][0].get()}, {variables["country"][0].get()}"
+    temp_list.insert(15, location)
+
+    doi = f"{variables["doi"][0].get()}"
+    temp_list.insert(16, doi)
+
+    url = f"{variables["url"][0].get()}"
+    temp_list.insert(16, url)
+
+    accessed = f"{variables["accessed"][0].get()}"
+    temp_list.insert(17, accessed)
+
+    formats = {
+            
+        author : "n",
+        ititle : "*i*",
+        title : "n",
+        pub: "n",
+        ipub: "*i*",
+        episode : "(b)",
+        edition : "n",
+        volume : "*i*",
+        issue : "*i*",
+        type: "[sb]",
+        source : "n",
+        isource : "*i*",
+        album : "n",
+        location : "n",
+        institution : "*i*",
+        time : "(b)",
+        pages : "(pp.)",
+        years : "(b)",
+        doi : "n",
+        url: "n",
+        accessed: "a"
+
+    }
+
+    for i in temp_list:
+        print(i)
+        print(formats[i])
+
+        if i != "" or i != "," or i != "-":
+            if formats[i] == "n":
+                f.insert(tk.END, i + ". ")
+            elif formats[i] == "*i*":
+                f.insert(tk.END, f"*{i}*")
+            elif formats[i] == "(b)":
+                f.insert(tk.END, f"({i})")
+            elif formats[i] == "[sb]":
+                f.insert(tk.END, f"[{i}]")
+            elif formats[i] == "(pp.)":
+                f.insert(tk.END, f"(pp. {i})")
+            elif formats[i] == "a":
+                f.insert(tk.END, f"Accessed {i}")
+            
+
+
+    f.config(state="disabled")
+
+
+def select_type(event):
+    option = sourcetype.get(sourcetype.curselection())
+
+    for widget in fields.winfo_children():
+        widget.grid_remove()
+
+    i = 0
+    fields_used = sourcetypes[option]
+    for field in fields_used:
+        i += 1
+        tk.Label(fields, text=field).grid(row=i, column=0)
+        for j, widget in enumerate(variables[fields_used[field]]):
+            widget.grid(row=i, column=j + 1)
+
+
+
+
+def select_style(event):
+    pass
+
+
+def render_citation(**kwargs):
+    pass
+
+
+root = tk.Tk()
+root.title("Citation Machine")
+root.geometry(f"960x720")
+
+general = tk.Frame(root)
+general.grid(row=1, column=0)
+
+fields = tk.Frame(root)
+fields.grid(row=8, column=0)
+
+
+"""
+Every field has
+- Label
+- Entry
+- variable
+
+{"variable": [Label, Entry]}
+"""
+
+variables = {
+    "author": [tk.Entry(fields)],
+    "ititle": [tk.Entry(fields)],
+    "title": [tk.Entry(fields)],
+    "subtitle": [tk.Entry(fields)],
+    "pub": [tk.Entry(fields)],
+    "ipub": [tk.Entry(fields)],
+    "episode": [tk.Entry(fields)],
+    "edition": [tk.Entry(fields)],
+    "volume": [tk.Entry(fields)],
+    "issue": [tk.Entry(fields)],
+    "type": [tk.Entry(fields)],
+    "source": [tk.Entry(fields)],
+    "isource": [tk.Entry(fields)],
+    "album": [tk.Entry(fields)],
+    "city": [tk.Entry(fields)],
+    "country": [tk.Entry(fields)],
+    "institution": [tk.Entry(fields)],
+    "date": [tk.Entry(fields)],
+    "sp": [tk.Entry(fields)],
+    "ep": [tk.Entry(fields)],
+    "sy": [tk.Entry(fields)],
+    "ey": [tk.Entry(fields)],
+    "doi": [tk.Entry(fields)],
+    "url": [tk.Entry(fields)],
+    "accessed": [tk.Entry(fields)],
+
+    "media": [tk.Radiobutton(fields, text="Post", value="post"),
+              tk.Radiobutton(fields, text="Comment", value="comment")],
+
+    "isep": [tk.Radiobutton(fields, text="Whole podcast", value="podcast"),
+                tk.Radiobutton(fields, text="Single episode", value="episode")],
+
+    "podtype": [tk.Radiobutton(fields, text="Audio", value="audio"),
+                tk.Radiobutton(fields, text="Video", value="video")],
+    
+    "degree": [tk.Radiobutton(fields, text="Thesis", value="thesis"),
+                tk.Radiobutton(fields, text="Dissertation", value="dissertation")]
+}
+
+sourcetypes = {
+    "Book": {
+        "Title": "ititle",
+        "Subtitle": "subtitle",
+        "Publisher": "pub",
+        "Edition": "edition",
+        "Volume": "volume",
+        "DOI": "doi"
+    },
+
+    "Article": {
+        "Article title": "title",
+        "Journal name": "isource",
+        "Volume No.": "volume",
+        "Issue No.": "issue",
+        "Start page": "sp",
+        "End page": "ep",
+        "DOI": "doi"
+    },
+
+    "Website": {
+        "Page title": "ititle",
+        "Website name": "source",
+        "Date accessed": "accessed"
+    },
+
+    "Blog post": {
+        "Post title": "title",
+        "Blog name": "isource",
+        "Publisher": "ipub",
+        "Date accessed": "accessed",
+    },
+
+    "Social media": {
+        "Post or comment": "media",
+        "Post title": "ititle",
+        "Content": "ititle",
+        "Website name": "source",
+        "Date accessed": "accessed"
+        
+    },
+
+    "Video": {
+        "Video title": "ititle",
+        "Website name": "source"
+    },
+
+    "Artwork": {
+        "Title": "ititle",
+        "Medium": "type",
+        "Museum or source": "source",
+        "City": "city",
+        "Country": "country"
+    },
+
+    "Image": {
+        "Title or description": "title",
+        "Medium": "type",
+        "Source": "source"
+    },
+
+    "Music": {
+        "Title": "title",
+        "Album": "album",
+        "Label": "source"
+    },
+
+    "Podcast": {
+        "Audio or video": "podtype",
+        "Episode or podcast": "isep",
+        "Podcast title": "isource",
+        "Episode title": "title",
+        "Episode number": "episode",
+        "Year started": "sy",
+        "Year ended": "ey",
+        "Production company": "source",
+    },
+
+    "Presentation": {
+        "Title": "ititle",
+        "Type of presentation": "type",
+        "Title of conference": "source",
+        "City": "city",
+        "Country": "country"
+    },
+
+    "Thesis or dissertation": {
+        "Thesis or dissertation": "degree",
+        "Title": "ititle",
+        "Institution": "institution",
+        "Source name": "source"
+    }
+}
+
+sourcetype = tk.Listbox(general, listvariable=tk.Variable(value=list(sourcetypes.keys())), selectmode="browse", height=len(sourcetypes), exportselection=False)
+sourcetype.bind("<<ListboxSelect>>", select_type)
+sourcetype.grid(row=1, column=0, padx=40, pady=40)
+
+"""
+style = tk.ttk.Combobox(general, values=["APA", "MLA", "Chicago"])
+style.bind("<<ComboboxSelected>>", select_style)
+style.grid(row=2, column=0)
+"""
+
+
+tk.Label(general, text="Author's first name").grid(row=3, column=2)
+tk.Label(general, text="Author's middle name").grid(row=4, column=2)
+tk.Label(general, text="Author's last name").grid(row=5, column=2)
+tk.Label(general, text="Author nickname").grid(row=6, column=2)
+tk.Label(general, text="Publication date").grid(row=4, column=4)
+tk.Label(general, text="URL").grid(row=5, column=4)
+
+
+fn = tk.Entry(general)
+fn.grid(row=3, column=3)
+
+mn = tk.Entry(general)
+mn.grid(row=4, column=3)
+
+ln = tk.Entry(general)
+ln.grid(row=5, column=3)
+
+nn = tk.Entry(general)
+nn.grid(row=6, column=3)
+
+date = tk.Entry(general)
+date.grid(row=4, column=5)
+
+url = tk.Entry(general)
+url.grid(row=5, column=5)
+
+textframe = tk.Frame(general)
+textframe.grid(row=1, column=2)
+
+
+f = tk.Text(textframe, wrap="word", width=20, height=10, state="disabled")
+f.grid(row=0, column=0)
+
+style = tk.OptionMenu(textframe, tk.StringVar(general, "Style..."), "APA", "MLA", "Chicago")
+style.grid(row=1, column=0, padx=10, pady=10)
+
+button = tk.Button(general, text="Update", command=get_entry)
+button.grid(row=7, column=2, padx=10, pady=10)
+
+
+root.mainloop()
