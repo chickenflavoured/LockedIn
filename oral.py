02b26d6d6149c4346c0a7e973364a4750ddbba37
Binh <105949566+chickenflavoured@users.noreply.github.com>
2026-04-15 13:58:58 -0400

    Update oral.py

--- oral.py
+++ oral.py
@@ -1,47 +1,54 @@
 import tkinter as tk
 import sounddevice as sd
 import numpy as np
+import threading
 from scipy.io import wavfile
-# Think abt using asyncio for recording in the background?
 
 root = tk.Tk()
 root.title("Oral Presentation")
 root.geometry("600x720")
 
+# https://python-sounddevice.readthedocs.io/en/latest/examples.html#recording-with-arbitrary-duration
+
 samplerate = 44100
 
 recording = np.array([])
+is_active = [False]
 
 def record():
+    is_active[0] = True
 
     print("Recording")
-    recording = sd.rec(4 * samplerate, samplerate=samplerate, channels=2)
-
-    sd.wait()
+    print(is_active)
+    while is_active[0]:
+        data = sd.rec(samplerate, samplerate=samplerate, channels=2)
+        np.append(recording, data)
     print("Done")
 
 
-def stop():
+def start_recording():
+    t = threading.Thread(target=record)
+    t.start()
 
-    recording = sd.stop()
-    print("Done")
+def end_recording():
+    is_active[0] = False
 
 
 def playback():
 
     print("Playing")
+    print(recording)
     sd.play(recording)
 
 
-start_button = tk.Button(root, text="Start recording", command=record)
-start_button.grid(row=1, column=0, padx=10, pady=0)
+rec_button = tk.Button(root, text="Record", command=record)
+rec_button.grid(row=1, column=0, padx=10, pady=0)
 
-end_button = tk.Button(root, text="Stop recording", command=stop)
-end_button.grid(row=2, column=0, padx=10, pady=0)
+rec_button = tk.Button(root, text="End", command=end_recording)
+rec_button.grid(row=1, column=1, padx=10, pady=0)
 
-play_button = tk.Button(root, text="Playback", command=playback)
-play_button.grid(row=3, column=0, padx=10, pady=0)
+play_button = tk.Button(root, text="Play", command=playback)
+play_button.grid(row=2, column=0, padx=10, pady=0)
 
 
 root.mainloop()
-
