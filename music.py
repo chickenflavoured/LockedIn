1507a33f025e2cf06e3e8dfcb9a1697949bd7412
Binh <105949566+chickenflavoured@users.noreply.github.com>
2026-04-20 21:43:34 -0400

    Update music.py

--- music.py
+++ music.py
@@ -2,88 +2,101 @@ import tkinter as tk
 import yt_dlp
 import os
 from yt_dlp.postprocessor import FFmpegPostProcessor
+import json
 
 import sdl2
 import sdl2.sdlmixer as mixer
 
 # https://pypi.org/project/PySDL2/
-FFmpegPostProcessor._ffmpeg_location.set("./ffmpeg/bin/ffmpeg.exe")
+#FFmpegPostProcessor._ffmpeg_location.set("./ffmpeg/bin/ffmpeg.exe")
 
 # Think about tkSnack? https://www.speech.kth.se/snack/
 
-mixer.Mix_OpenAudio(44100, sdl2.AUDIO_S16SYS, 2, 2048)
 
-def play(event):
-    if mixer.Mix_Playing(-1):
-        mixer.Mix_HaltChannel(-1)
+class Player:
 
-    print(playlist.get(playlist.curselection()))
-    file = mixer.Mix_LoadWAV(f"./tracks/{playlist.get(playlist.curselection())}".encode("utf-8"))
-    mixer.Mix_PlayChannel(-1, file, 0)
+    def __init__(self, playlist: dict = {}):
+        mixer.Mix_OpenAudio(44100, sdl2.AUDIO_S16SYS, 2, 2048)
+        self.playlist = playlist
+        self.counter = 0
 
-def resume():
-    mixer.Mix_Resume(-1)
+    def play(self, event):
+        if mixer.Mix_Playing(-1):
+            mixer.Mix_HaltChannel(-1)
+        
+        selection = playlist_list.get(playlist_list.curselection())
 
+        print(playlist_list.get(playlist_list.curselection()))
+        file = mixer.Mix_LoadWAV(f"./tracks/{self.playlist[selection]}.mp3".encode("utf-8"))
+        mixer.Mix_PlayChannel(-1, file, 0)
 
-def pause():
-    mixer.Mix_Pause(-1)
-
-
-def stop():
-    mixer.Mix_FadeOutChannel(-1, 1000)
-
-# https://youtu.be/_RGp-Cynxkg
-def download():
-
-    url = link.get()
-
-    ydl_options = {
-        "outtmpl": f"tracks/{len(os.listdir('./tracks'))}",
-        "format": "bestaudio/best",
-        "ffmpeg-location": "./ffmpeg/bin/ffmpeg.exe",
-        # "ffmpeg-location": "ffmpeg/bin/ffmpeg.exe",
-        "postprocessors": [{
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "44.1"
-        }]
-        }
+    def resume(self):
+        mixer.Mix_Resume(-1)
 
 
-    with yt_dlp.YoutubeDL(ydl_options) as ydl:
-        ydl.download([url])
+    def pause(self):
+        mixer.Mix_Pause(-1)
 
-        info = ydl.extract_info(url, download=False)
-        title = info.get("title", None)
-        duration = info.get("duration", None)
-        views = info.get("view_count", None)
-    
-    playlist.config(listvariable=tk.Variable(value=os.listdir("./tracks")), height=len(os.listdir("./tracks")))
+
+    def stop(self):
+        mixer.Mix_FadeOutChannel(-1, 500)
+
+    # https://youtu.be/_RGp-Cynxkg
+    def download(self):
+
+        url = link.get()
+
+        ydl_options = {
+            "outtmpl": f"tracks/{self.counter}",
+            "format": "bestaudio/best",
+            # "ffmpeg-location": "./ffmpeg/bin/ffmpeg.exe",
+            "ffmpeg-location": "ffmpeg/bin/ffmpeg.exe",
+            "postprocessors": [{
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192"
+            }]
+            }
+
+
+        with yt_dlp.YoutubeDL(ydl_options) as ydl:
+            ydl.download([url])
+
+            info = ydl.extract_info(url, download=False)
+            title = info.get("title", None)
+            duration = info.get("duration", None)
+            views = info.get("view_count", None)
+        self.playlist[title] = self.counter
+        json.dump(self.playlist, "playlistdata.json")
+
+        self.counter += 1
+
+        playlist_list.config(listvariable=tk.Variable(value=list(self.playlist.keys())), height=len(self.playlist))
 
 
 root = tk.Tk()
 root.title("Music Player")
 root.geometry("480x360")
 
-is_playing = False
-
 link = tk.Entry(root)
 link.grid(row=0, column=0)
 
-play_button = tk.Button(root, text="Resume", command=resume)
+player = Player(json.load("playlistdata.json"))
+
+play_button = tk.Button(root, text="Resume", command=player.resume)
 play_button.grid(row=1, column=0, padx=10, pady=0)
 
-pause_button = tk.Button(root, text="Pause", command=pause)
+pause_button = tk.Button(root, text="Pause", command=player.pause)
 pause_button.grid(row=2, column=0, padx=10, pady=0)
 
-stop_button = tk.Button(root, text="Stop", command=stop)
+stop_button = tk.Button(root, text="Stop", command=player.stop)
 stop_button.grid(row=3, column=0, padx=10, pady=0)
 
-dl_button = tk.Button(root, text="Download", command=download)
+dl_button = tk.Button(root, text="Download", command=player.download)
 dl_button.grid(row=1, column=1, padx=10, pady=0)
 
-playlist = tk.Listbox(root, listvariable=tk.Variable(value=os.listdir("./tracks")), selectmode="browse", height=len(os.listdir("./tracks")), exportselection=False)
-playlist.bind("<<ListboxSelect>>", play)
-playlist.grid(row=4, column=0, padx=40, pady=40)
+playlist_list = tk.Listbox(root, listvariable=tk.Variable(value=list(player.playlist.keys())), height=len(player.playlist), exportselection=False)
+playlist_list.bind("<<ListboxSelect>>", player.play)
+playlist_list.grid(row=4, column=0, padx=40, pady=40)
 
 root.mainloop()
