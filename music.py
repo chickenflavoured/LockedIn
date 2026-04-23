5b917fe051f0a5190763aa568f1ccd98045391a7
Binh <105949566+chickenflavoured@users.noreply.github.com>
2026-04-23 12:45:05 -0400

    Update music.py

--- music.py
+++ music.py
@@ -6,6 +6,7 @@ import json
 
 import sdl2
 import sdl2.sdlmixer as mixer
+from tkextrafont import Font
 
 # https://pypi.org/project/PySDL2/
 #FFmpegPostProcessor._ffmpeg_location.set("./ffmpeg/bin/ffmpeg.exe")
@@ -20,6 +21,7 @@ class Player:
         self.playlist = playlist
         self.counter = 0
 
+
     def play(self, event):
         if mixer.Mix_Playing(-1):
             mixer.Mix_HaltChannel(-1)
@@ -29,6 +31,14 @@ class Player:
         print(playlist_list.get(playlist_list.curselection()))
         file = mixer.Mix_LoadWAV(f"./tracks/{self.playlist[selection]}.mp3".encode("utf-8"))
         mixer.Mix_PlayChannel(-1, file, 0)
+    
+
+    def delete(self):
+        selection = playlist_list.get(playlist_list.curselection())
+        self.playlist.pop(selection)
+
+        playlist_list.config(listvariable=tk.Variable(value=list(self.playlist.keys())), height=len(self.playlist))
+
 
     def resume(self):
         mixer.Mix_Resume(-1)
@@ -67,7 +77,8 @@ class Player:
             duration = info.get("duration", None)
             views = info.get("view_count", None)
         self.playlist[title] = self.counter
-        json.dump(self.playlist, "playlistdata.json")
+        with open("playlistdata.json", "w") as f:
+            json.dump(self.playlist, f)
 
         self.counter += 1
 
@@ -78,10 +89,21 @@ root = tk.Tk()
 root.title("Music Player")
 root.geometry("480x360")
 
+root.resizable(width=False, height=False)
+
+# THIS SETS THE FONT
+font = Font(file="sniglet.ttf", family="Sniglet")
+root.option_add("*Font", "Sniglet 10")
+
+bg = tk.PhotoImage(file = "music.png")
+bg_label = tk.Label(root, image=bg)
+bg_label.place(x = 0, y = 0)
+
 link = tk.Entry(root)
 link.grid(row=0, column=0)
 
-player = Player(json.load("playlistdata.json"))
+with open("playlistdata.json") as f:
+    player = Player(json.load(f))
 
 play_button = tk.Button(root, text="Resume", command=player.resume)
 play_button.grid(row=1, column=0, padx=10, pady=0)
@@ -92,11 +114,18 @@ pause_button.grid(row=2, column=0, padx=10, pady=0)
 stop_button = tk.Button(root, text="Stop", command=player.stop)
 stop_button.grid(row=3, column=0, padx=10, pady=0)
 
+del_button = tk.Button(root, text="Delete", command=player.delete)
+del_button.grid(row=4, column=0, padx=10, pady=0)
+
 dl_button = tk.Button(root, text="Download", command=player.download)
 dl_button.grid(row=1, column=1, padx=10, pady=0)
 
-playlist_list = tk.Listbox(root, listvariable=tk.Variable(value=list(player.playlist.keys())), height=len(player.playlist), exportselection=False)
+playlist_list = tk.Listbox(root, 
+                           listvariable=tk.Variable(value=list(player.playlist.keys())), 
+                           width=35,
+                           height=len(player.playlist), 
+                           exportselection=False)
 playlist_list.bind("<<ListboxSelect>>", player.play)
-playlist_list.grid(row=4, column=0, padx=40, pady=40)
+playlist_list.grid(row=5, column=0, padx=40, pady=40)
 
 root.mainloop()
